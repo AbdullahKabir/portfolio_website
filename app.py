08dad40b6e7b29d33f581ba9747ad1f4ab2fc2ef
Abdullah Kabir <abdullah.kabir12@gmail.com>
2025-12-27 11:28:50 +0100

    gif change

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
         <div class="container">
             <div class="row min-vh-100 align-items-center justify-content-center">
                 <div class="col-12 text-center">
-                    <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnh4Z3RldHN0cG1qcW1yenRnbnEycnJtZnRsOWx5N2hxanN6a2NjaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7bu3XilJ5BOiSGic/giphy.gif" 
+                    <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHEwYmI4b2U5emFteHMwMnVhd2IybjJmYWIzMmVtZzR2ZzR1aGUwbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H54iQXnwulV0sFq93w/giphy.gif" 
                          alt="Under Construction" 
                          class="img-fluid">
                     <h1 class="mt-4">Site Under Construction</h1>
